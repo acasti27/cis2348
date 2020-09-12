b92262c38107d143045bb0c5cefa2ea995c67ea0
acasti27 <70541336+acasti27@users.noreply.github.com>
2020-09-12 15:15:03 -0500

    Zylabs 1.12

---
+++ Homework 1/main.py
@@ -0,0 +1,4 @@
+# Alejandra Castillo
+userNum = int(input()) userNumSquared = userNum + userNum # Bug here; fix it when instructed
+
+print(userNumSquared, end=' ') # Output formatting issue here; fix it when instructed
