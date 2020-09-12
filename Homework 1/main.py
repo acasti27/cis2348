856c30ec1f9a678afe6647772a465dd2009865fc
acasti27 <70541336+acasti27@users.noreply.github.com>
2020-09-12 18:53:46 -0500

    Zylab 1.20

--- Homework 1/main.py
+++ Homework 1/main.py
@@ -1,18 +1,20 @@
 # Alejandra Castillo 1440370
 
-user_num1 = input('Enter integer:')
+print('Enter integer:')
+user_num1 = input()
 print('You entered:', user_num1)
 
 squared = int(user_num1) * int(user_num1)
 cubed = int(user_num1) * int(user_num1) * int(user_num1)
 
 print(user_num1, 'squared is', squared)
+
 print('And', user_num1, 'cubed is', cubed, '!!')
+print('Enter another integer:')
 
-user_num2 = input('Enter another integer:')
-
-product_sum = int(user_num1) + int(user_num2)
-product_pro = int(user_num1) * int(user_num2)
+user_num2 = input()
+product_sum = int(user_num1)+int(user_num2)
+product_pro = int(user_num1)*int(user_num2)
 
 print(user_num1, '+', user_num2, 'is', product_sum)
 print(user_num1, '*', user_num2, 'is', product_pro)
