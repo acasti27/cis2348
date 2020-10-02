a317b1ea3b5ab3e2411112da762d33bc69df21dc
acasti27 <70541336+acasti27@users.noreply.github.com>
2020-10-02 16:59:01 -0500

    Homework coding problem

--- Homework 2/hwproblem.py
+++ Homework 2/hwproblem.py
@@ -1,21 +1,23 @@
 # Alejandra Castillo 1440370
 # Creating a dictionary for the months
 months_list = {
-    'january': 1,
-    'february': 2,
-    'march': 3,
-    'april': 4,
-    'may': 5,
-    'june': 6,
-    'july': 7,
-    'august': 8,
-    'september': 9,
-    'october': 10,
-    'november': 11,
-    'december': 12
+    'january': '1',
+    'february': '2',
+    'march': '3',
+    'april': '4',
+    'may': '5',
+    'june': '6',
+    'july': '7',
+    'august': '8',
+    'september': '9',
+    'october': '10',
+    'november': '11',
+    'december': '12'
 }
+# opens files
+in_file = open('C:\\Users\\Desktop\\inputDates.txt')
+out_file = open('C:\\Users\\Desktop\\parsedDates.txt')
 # splitting the strings of data inputted
-in_file = open('C:\\User\\Desktop\\inputDates.txt')
 for date in in_file:
     if date != '-1':
         lis = date.split()
@@ -27,7 +29,11 @@ for date in in_file:
             if month.lower() in months_list:
                 comma = day[-1]
                 day = day[:len(day)-1]
-                month_number = months_list(month.lower())
+                month_number = months_list[month.lower()]
                 ans = month_number+'/'+day+'/'+year
                 print(ans)
-
+                out_file.write(ans)
+                out_file.write('\n')
+# closes file
+out_file.close()
+in_file.close()
