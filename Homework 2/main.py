ee48794af75cb6e72ed62b44f343cbb126ff32af
acasti27 <70541336+acasti27@users.noreply.github.com>
2020-10-02 18:02:15 -0500

    zylab 6.22

---
+++ Homework 2/main.py
@@ -0,0 +1,10 @@
+# Alejandra Castillo 1440370
+# brute force equation solver
+# declaring the variables
+a = int(input())
+b = int(input())
+c = int(input())
+x = int(input())
+y = int(input())
+z = int(input())
+
