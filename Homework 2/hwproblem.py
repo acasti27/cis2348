fe363905a503d86889e23d4a289dc8ba4b3b9144
acasti27 <70541336+acasti27@users.noreply.github.com>
2020-10-02 16:13:55 -0500

    Homework coding problem

---
+++ Homework 2/hwproblem.py
@@ -0,0 +1,25 @@
+# Alejandra Castillo 1440370
+# Creating a dictionary for the months
+months = {
+    'january': 1,
+    'february': 2,
+    'march': 3,
+    'april': 4,
+    'may': 5,
+    'june': 6,
+    'july': 7,
+    'august': 8,
+    'september': 9,
+    'october': 10,
+    'november': 11,
+    'december': 12
+}
+# splitting the strings of data inputted
+in_file = open('C:\\User\\Desktop\\inputDates.txt')
+for date in in_file:
+    if date != '-1':
+        lis = date.split()
+        if len(lis) >= 3:
+            month = lis[0]
+            day = lis[1]
+            year = lis[2]
