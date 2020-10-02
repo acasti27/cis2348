e2e8b57adbbc56bd344a7517f58e69c1cebc80f1
acasti27 <70541336+acasti27@users.noreply.github.com>
2020-10-02 18:15:50 -0500

    zylab 6.22

--- Homework 2/main.py
+++ Homework 2/main.py
@@ -4,7 +4,17 @@
 a = int(input())
 b = int(input())
 c = int(input())
-x = int(input())
-y = int(input())
+u = int(input())
+w = int(input())
 z = int(input())
-
+# declaration if no solution is found
+found_solution = False
+# implementing the brute force approach
+for x in range(-10, 11):
+    for y in range(-10, 11):
+        if a * x + b * y == c and u * x + w * y == z:
+            print(x, y)
+            found_solution = True
+# alternative solution if none is found
+if not found_solution:
+    print('No solution')
