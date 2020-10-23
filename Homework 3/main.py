a6de332a72ea64f44444efb79af72355aea0c877
acasti27 <70541336+acasti27@users.noreply.github.com>
2020-10-22 20:32:30 -0500

    zylab 10.17

--- Homework 3/main.py
+++ Homework 3/main.py
@@ -1,2 +1,12 @@
 # Alejandra Castillo 1440370
 # Online shopping cart (Part 1)
+
+class ItemToPurchase:
+    def __init__(self):
+        self.item_name = 'none'
+        self.item_price = 0.0
+        self.item_quantity = 0
+
+    def print_item_cost(self):
+        print(self.item_name, '', str(self.item_quantity), '@$', str(self.item_price), '=$', str(self.item_price
+              * self.item_quantity))
