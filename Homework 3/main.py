35e52a38f39993bb842b88280261168b21569dec
acasti27 <70541336+acasti27@users.noreply.github.com>
2020-10-22 20:13:30 -0500

    zylab 10.17

--- Homework 3/main.py
+++ Homework 3/main.py
@@ -1,14 +1,2 @@
 # Alejandra Castillo 1440370
-# Winning team (classes)
-
-# class definition
-class Team:
-    def __init__(self):
-        self.team_name = 'none'
-        self.team_wins = 0
-        self.team_losses = 0
-
-    # TODO: Define get_win_percentage()
-    def get_win_percentage(self):
-        percent = self.team_wins / (self.team_wins + self.team_losses)
-        return percent
+# Online shopping cart (Part 1)
