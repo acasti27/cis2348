6c171e039b3f59ef5812770780e6beb2264395bc
acasti27 <70541336+acasti27@users.noreply.github.com>
2020-10-22 19:41:57 -0500

    zylab 10.15

---
+++ Homework 3/main.py
@@ -0,0 +1,9 @@
+# Alejandra Castillo 1440370
+# Winning team (classes)
+
+# class definition
+class Team:
+    def __init__(self):
+        self.team_name = 'none'
+        self.team_wins = 0
+        self.team_losses = 0
