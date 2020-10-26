58505eac4ccea4ed4084cde13c9f5f314819adea
acasti27 <70541336+acasti27@users.noreply.github.com>
2020-10-26 16:13:32 -0500

    zylab 11.27

--- Homework 3/main.py
+++ Homework 3/main.py
@@ -1,6 +1,16 @@
 # Alejandra Castillo 1440370
 # Soccer team roster (Dictionaries)
 
+# rearranged the keys to define the roster
+def print_roster():
+    keys = list(players.keys())
+    keys.sort()
+    # prints out the roster sorted
+    print('ROSTER')
+    for key in keys:
+        print('Jersey number: %d, Rating: %d' % (key, players[key]))
+
+
 # dictionary for players
 players = {}
 # prompts the user to enter up to five jersey numbers and ratings
@@ -9,17 +19,14 @@ for i in range(5):
     players[jersey_num] = int(input("Enter player %d's rating: \n" % (i+1)))
     print('')
 
-keys = list(players.keys())
-keys.sort()
-# prints out the roster sorted
-print('ROSTER')
-for key in keys:
-    print('Jersey number: %d, Rating: %d' % (key, players[key]))
 
 # prints out a menu with quitting being the only working option to select
+# added option o as a working option
 while True:
     print('\n MENU \n a - Add player \n d - Remove player \n u - Update player rating'
           '\n r - Output players above a rating \n o - Output roster \n q - Quit')
     option = input('\n Choose an option:')
+    if option == 'o':
+        print_roster()
     if option == 'q':
         break
