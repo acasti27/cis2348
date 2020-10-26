045ca866634df4923514b5c58d73664652006502
acasti27 <70541336+acasti27@users.noreply.github.com>
2020-10-26 17:26:01 -0500

    zylab 11.27

---
+++ Homework 3/zylabseven.py
@@ -0,0 +1,55 @@
+# Alejandra Castillo 1440370
+# Soccer team roster (Dictionaries)
+
+# rearranged the keys to define the roster
+def print_roster():
+    keys = list(players.keys())
+    keys.sort()
+    # prints out the roster sorted
+    print('ROSTER')
+    for key in keys:
+        print('Jersey number: %d, Rating: %d' % (key, players[key]))
+
+
+# dictionary for players
+players = {}
+# prompts the user to enter up to five jersey numbers and ratings
+for i in range(5):
+    jersey_num = int(input("Enter player %d's jersey number:\n" % (i+1)))
+    players[jersey_num] = int(input("Enter player %d's rating:\n" % (i+1)))
+    print('')
+keys = list(players.keys())
+keys.sort()
+print('ROSTER')
+for key in keys:
+    print('Jersey number: %d, Rating: %d' % (key, players[key]))
+# prints out a menu with quitting being the only working option to select
+# added option o as a working option
+# added a as a working option
+# added d as a working option
+# added u as a working option
+while True:
+    print('\nMENU\na - Add player\nd - Remove player\nu - Update player rating'
+          '\nr - Output players above a rating\no - Output roster\nq - Quit')
+    option = input('\nChoose an option:\n')
+    if option == 'o':
+        print_roster()
+    elif option == 'a':
+        jersey_num = int(input("Enter a new player's jersey number:\n"))
+        rating = int(input("Enter the player's rating:\n"))
+        players[jersey_num] = rating
+    elif option == 'd':
+        jersey_num = int(input("Enter a player's jersey number:\n"))
+        if jersey_num in list(players.keys()):
+            del players[jersey_num]
+    elif option == 'u':
+        jersey_num = int(input("Enter a player's jersey number:\n"))
+        rating = int(input('Enter a new rating for player:\n)'))
+        players[jersey_num] = rating
+    elif option == 'r':
+        rating = int(input('Enter a rating:\n'))
+        keys = list(players.keys())
+        keys.sort()
+        print('\n ABOVE %d' % rating)
+    if option == 'q':
+        break
