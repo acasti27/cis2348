1e1138d6e529804b8cf9d6f10c0413de5aa53f67
acasti27 <70541336+acasti27@users.noreply.github.com>
2020-10-28 19:25:32 -0500

    zylab  10.19 - restarting the code and adding on to the ItemtoPurchase class

---
+++ Homework 3/main.py
@@ -0,0 +1,11 @@
+# Alejandra Castillo 1440370
+# Online shopping cart (Part 2)
+
+# defining the class for purchasing items
+# adding on item description
+class ItemToPurchase:
+    def __init__(self):
+        self.item_name = 'none'
+        self.item_price = 0.0
+        self.item_quantity = 0
+        self.item_description = 'none'
