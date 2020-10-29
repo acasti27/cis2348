880536505a78e9b90cbac83fbd42be9f97234000
acasti27 <70541336+acasti27@users.noreply.github.com>
2020-10-28 19:45:22 -0500

    zylab  10.19 - defining shopping cart class

--- Homework 3/main.py
+++ Homework 3/main.py
@@ -19,3 +19,11 @@ class ItemToPurchase:
 # will print out in the order of item name and then description
     def print_item_description(self):
         print(self.item_name + ':' + self.item_description)
+
+
+# defining the shopping cart class
+class ShoppingCart:
+    def __init__(self):
+        self.customer_name = 'none'
+        self.current_date = 'January 1,2016'
+        self.cart_items = []
