380ccbadbf2984666d35942d3969e72df4cfb93a
acasti27 <70541336+acasti27@users.noreply.github.com>
2020-11-13 17:55:17 -0600

    Final Project part 1 - input files are able to be read

--- Final Project/FinalProject_MainPart1.py
+++ Final Project/FinalProject_MainPart1.py
@@ -1,13 +1,18 @@
 # Alejandra Castillo 1440370
 # This is the first part of my final project
-# In theory, this should be able to read any csv file and output either
+# In theory, this should be able to read any csv file and output either an
 # alphabetically sorted, by item id, old to recent, or most expensive to least
-# expensive
+# expensive list
 #
-# Referencing Zylab - this will initiate the use of csv readers
+# Referencing Zylab Ch.9 - this will initiate the use of csv readers
 import csv
 # var name for general input file
 input_file = input()
 # this will read any input file
-with open(input_file, 'File') as csvfile:
+with open(input_file, 'r') as csvfile:
     csv_reader = csv.reader(csvfile)
+# now to set some conditions for which the file should be read and outputted
+    # specifically, this will be reading each row at a time and then reading each value or column
+    # First I'm checking to make sure that the file can be read
+    for row in csv_reader:
+        print(row)
