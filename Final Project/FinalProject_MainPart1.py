c798ba921f035f0a21e9008be9a48fbce5083c38
acasti27 <70541336+acasti27@users.noreply.github.com>
2020-11-13 17:10:39 -0600

    Final Project part 1 - beginning with csv import

--- Final Project/FinalProject_MainPart1.py
+++ Final Project/FinalProject_MainPart1.py
@@ -1,3 +1,8 @@
 # Alejandra Castillo 1440370
 # This is the first part of my final project
-# In theory, this should be able to read any csv file
+# In theory, this should be able to read any csv file and output either
+# alphabetically sorted, by item id, old to recent, or most expensive to least
+# expensive
+#
+# Referencing Zylab - this will initiate the use of csv readers
+import csv
