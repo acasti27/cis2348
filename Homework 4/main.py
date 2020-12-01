7663c7c26c60a6e8826ebdfa24b298cce4f431d8
acasti27 <70541336+acasti27@users.noreply.github.com>
2020-12-01 17:44:27 -0600

    Zylab 12.7 - rearranged everything

--- Homework 4/main.py
+++ Homework 4/main.py
@@ -2,8 +2,9 @@
 # Fat-burning heart rate
 # defining the fat burning heart rate
 def fat_burning_heart_rate(age):
-    heart_rate = (70 / 100 * 220) - adult_age
     # adding age input into def
     adult_age = int(input())
-    if adult_age < 18 or > 75 :
+    heart_rate = (70 / 100 * 220) - adult_age
+    if adult_age < 18 or adult_age > 75:
         raise ValueError('Invalid age.')
+    return heart_rate, adult_age
