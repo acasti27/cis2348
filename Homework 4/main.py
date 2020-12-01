0bb8dc5b14df03a6cbba3308617004d6b021d8ef
acasti27 <70541336+acasti27@users.noreply.github.com>
2020-12-01 17:46:35 -0600

    Zylab 12.7 - adding main

--- Homework 4/main.py
+++ Homework 4/main.py
@@ -5,6 +5,10 @@ def fat_burning_heart_rate(age):
     # adding age input into def
     adult_age = int(input())
     heart_rate = (70 / 100 * 220) - adult_age
+    # this will set the parameter for the value error
     if adult_age < 18 or adult_age > 75:
         raise ValueError('Invalid age.')
     return heart_rate, adult_age
+
+# adding in the main
+if __name__ == '__main__':
