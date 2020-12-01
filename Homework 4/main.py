3740bfa6d962596cfa3ba67ab11e86b145e9fa64
acasti27 <70541336+acasti27@users.noreply.github.com>
2020-12-01 17:41:11 -0600

    Zylab 12.7 - wrote age into def for heart rate and adding the value error

--- Homework 4/main.py
+++ Homework 4/main.py
@@ -2,4 +2,8 @@
 # Fat-burning heart rate
 # defining the fat burning heart rate
 def fat_burning_heart_rate(age):
-    heart_rate = (70/100 * 220) - adult_age
+    heart_rate = (70 / 100 * 220) - adult_age
+    # adding age input into def
+    adult_age = int(input())
+    if adult_age < 18 or > 75 :
+        raise ValueError('Invalid age.')
