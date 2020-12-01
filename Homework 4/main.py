951cc3d8b56ebee65915dc67ccb9d42fa8795d42
acasti27 <70541336+acasti27@users.noreply.github.com>
2020-12-01 17:13:00 -0600

    Zylab 12.7 - adding in a definition for age as well as conditions

--- Homework 4/main.py
+++ Homework 4/main.py
@@ -3,4 +3,15 @@
 # defining the fat burning heart rate
 def fat_burning_heart_rate():
     heart_rate = (70/100 * 220) - adult_age
+    # might be incorrect way to write in formula
 
+
+# defining age input
+def age():
+    adult_age = int(input())
+# if else statement for what to do if the age is within parameters
+    if age >= 18 and <= 75:
+        print(adult_age)
+    # inluding the value error
+    else:
+        raise ValueError('Invalid age.')
