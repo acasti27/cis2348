928bd20f28cfad3735750e4d6ce21df0e50a27f1
acasti27 <70541336+acasti27@users.noreply.github.com>
2020-11-30 20:28:26 -0600

    Final Project part 2- user input prompted

--- Final Project/FinalProject_MainPart2.py
+++ Final Project/FinalProject_MainPart2.py
@@ -1,2 +1,5 @@
 # Alejandra Castillo 1440370
 # This is the second part of my final project
+# prompting the user to enter a manufacturer and an item type
+input_one = input('Please enter a manufacturer:')
+input_two = input('Please enter an item type:')
