5821de0cfd52fbad1992da69830b9a9c73717aa4
acasti27 <70541336+acasti27@users.noreply.github.com>
2020-12-01 19:10:40 -0600

    Zylab 14.11 - defined how it would be sorted

---
+++ Homework 4/main.py
@@ -0,0 +1,22 @@
+# Alejandra Castillo 1440370
+# Descending selection sort with output during execution
+# TODO: Write a selection_sort_descend_trace() function that
+#       sorts the numbers list into descending order
+def selection_sort_descend_trace(numbers):
+    # defined the sorting
+    for a in range(len(numbers) - 1):
+        ind = a
+        for b in range(a +1, len(numbers)):
+            if numbers[b] > numbers[ind]:
+                ind = b
+        numbers[a], numbers[ind] = numbers[ind], numbers[a]
+        for x in numbers:
+            print(x, end='')
+            print()
+
+
+if __name__ == "__main__":
+    # TODO: Read in a list of integers into numbers, then call
+    #       selection_sort_descend_trace() to sort the numbers
+    numbers = [int(x) for x in input().split()]
+    selection_sort_descend_trace(numbers)
