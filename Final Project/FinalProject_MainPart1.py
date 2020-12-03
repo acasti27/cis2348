adcb5dd361839a3ae0337223be016b51ec91083f
acasti27 <70541336+acasti27@users.noreply.github.com>
2020-12-03 12:54:34 -0600

    Final Project Part 1 - added a menu

--- Final Project/FinalProject_MainPart1.py
+++ Final Project/FinalProject_MainPart1.py
@@ -7,12 +7,18 @@
 # Referencing Zylab Ch.9 - this will initiate the use of csv readers
 import csv
 # var name for general input file
+# decided to ask for the user to enter a file name
 input_file = input('Please enter a file name:')
+# Will move around later, but I wanted to include a list of choices to give myself a better idea on how to go about the
+# rest of this part
+print('\nMenu\na - Sort alphabetically\ns - Sort by item id\no - Sort by oldest to newest\ne - Sort by most expensive'
+      'to least\nq = Quit')
+
 # this will read any input file
 with open(input_file, 'r') as csvfile:
     csv_reader = csv.reader(csvfile)
 # now to set some conditions for which the file should be read and outputted
     # specifically, this will be reading each row at a time and then reading each value or column
     # First I'm checking to make sure that the file can be read
-    for row in csv_reader:
-        print(row)
+#    for row in csv_reader:
+#        print(row)
