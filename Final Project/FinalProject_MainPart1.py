b4e11c893aa5255318463030fa4724c7f2a10ef6
acasti27 <70541336+acasti27@users.noreply.github.com>
2020-12-03 18:31:19 -0600

    Final Project part 1 - defining the menu you try to get it to work with options

--- Final Project/FinalProject_MainPart1.py
+++ Final Project/FinalProject_MainPart1.py
@@ -22,10 +22,8 @@ with open(input_file, 'r') as csvfile:
         print(row)
 # letting the file print out for now
 
-
+# defining a class to create a menu
 def print_menu(obj):
     menu = ('\nMenu\na - Sort alphabetically\ns - Sort by item id\no - Sort by oldest to newest\ne - Sort by most expensive'
       'to least\nq = Quit')
-# moving my menu down for now while I work things out
-print('\nMenu\na - Sort alphabetically\ns - Sort by item id\no - Sort by oldest to newest\ne - Sort by most expensive'
-      'to least\nq = Quit')
+
