578893763b20a600b927d46cbc89deb5c3ed0d61
acasti27 <70541336+acasti27@users.noreply.github.com>
2020-12-03 22:23:37 -0600

    Final Project part 1 - creating classes

--- Final Project/FinalProject_MainPart1.py
+++ Final Project/FinalProject_MainPart1.py
@@ -3,23 +3,27 @@
 # In theory, this should be able to read any csv file and output either an
 # alphabetically sorted, by item id, old to recent, or most expensive to least
 # expensive list
-
-# Referencing Zylab Ch.9 - this will initiate the use of csv readers
-import csv
+# defining a class solely for reading the file
+class CsvReading:
+    import csv
+# Referencing Zylab Ch.9 - this will initiate the use of csv reader
 # var name for general input file
 # decided to ask for the user to enter a file name
-input_file = input('Please enter a file name:')
+    input_file = input('Please enter a file name:')
 # Will move around later, but I wanted to include a list of choices to give myself a better idea on how to go about the
 # rest of this part
-sort = {}
-
+    sort = {}
 # this will read any input file
-with open(input_file, 'r') as csvfile:
-    csv_reader = csv.reader(csvfile)
-# now to set some conditions for which the file should be read and outputted
-    # specifically, this will be reading each row at a time and then reading each value or column
-    # First I'm checking to make sure that the file can be read
-    for row in csv_reader:
-        print(row)
+    with open(input_file, 'r') as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=',')
+        # now to set some conditions for which the file should be read and outputted
+        # specifically, this will be reading each row at a time and then reading each value or column
+        # First I'm checking to make sure that the file can be read
+        for row in csv_reader:
+            print(row)
 # letting the file print out for now
+
+# defining a class for sorting the file alphabetically
+class AlphabetSort:
+    def __init__(self, item_id, manufacturer, item_type, price, manu_date, condition):
 
