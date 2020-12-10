f1c1303bbced28a93c4e6a5ba059d8d8f94db19b
acasti27 <70541336+acasti27@users.noreply.github.com>
2020-12-10 13:40:03 -0600

    Final Project part 2 - moved some things around

--- Final Project/FinalProject_MainPart2.py
+++ Final Project/FinalProject_MainPart2.py
@@ -1,17 +1,19 @@
 # Alejandra Castillo 1440370
 # This is the second part of my final project
+# need to figure out how to get this to read three files
+# this will read all the csv files
+import csv
+# this is for the group of files
+csv_files = []
 
-class CReader:
-
+# this class will be for reading the files
+class CSVReader:
     # importing csv to read the file
-    import csv
-
-    csv_content = []
     # prompting the user to input a file name
-    input_file = input('Please enter a file name:')
+    input_file = input('Please enter a file name:\n')
     # letting the file print out for now
     with open(input_file, 'r') as csvfile:
-        csv_reader = csv.reader(csvfile)
+        csv_reader = csv.reader(csvfile, delimiter=',')
         for row in csv_reader:
             print(row)
 
@@ -24,21 +26,18 @@ class CReader:
         # prompting the user to enter a manufacturer and an item type
         input_one = input('Please enter a manufacturer:')
         input_two = input('Please enter an item type:')
-        if manufacturer and item_type is not in CReader:
+        if manufacturer and item_type not in CSVReader:
             # this will print out whether or not the specified items are found
             return 'No such item in inventory!'
         else:
             # this is the format to how it should be printing out if it is found within the csv file
-            return 'Your item is: {}, {}, {}, {}'.format(self.item_id, self.manufacterer, self.item_type, self.price)
-            return 'You may, also, consider: {} {}'
+            print('Your item is: {}, {}, {}, {}').format(self.item_id, self.manufacterer, self.item_type, self.price)
+            print('You may, also, consider: {} {}, {}, {}').format(self.item_id, self.manufacter, self.item_type, self.price)
 
     # trying to loop back as a simple menu
-    def print_question():
+    def print_question(self):
         print('\nWould you like to search for something else?\ny - Yes\nn - no (quit)')
         option = input('\nPlease select an option:\n')
         if option == 'y':
             print(check_inventory)
         if option == 'n':
-            break
-
-
