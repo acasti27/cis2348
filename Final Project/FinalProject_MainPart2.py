30a944793ddf6d2905c16ebc1cfaab35ac39fbdc
acasti27 <70541336+acasti27@users.noreply.github.com>
2020-12-10 21:36:50 -0600

    Final Project part 2 - redoing everything to try to get things more organized

--- Final Project/FinalProject_MainPart2.py
+++ Final Project/FinalProject_MainPart2.py
@@ -1,43 +1,39 @@
 # Alejandra Castillo 1440370
 # This is the second part of my final project
-# need to figure out how to get this to read three files
+
 # this will read all the csv files
 import csv
-# this is for the group of files
+# this is for the group of files to append to
 csv_files = []
+manufacturer = []
+prices = []
+service_dates = []
+
 
 # this class will be for reading the files
 class CSVReader:
     # importing csv to read the file
     # prompting the user to input a file name
+    print('When entering group of files, please enter in the following order: '
+          'Manufacturer, Service Date, Price List')
     input_file = input('Please enter a file name:\n')
+    file_two = input('Please enter a second file name:\n')
+    file_three = input('Please enter a third file name:\n')
     # letting the file print out for now
     with open(input_file, 'r') as csvfile:
         csv_reader = csv.reader(csvfile, delimiter=',')
         for row in csv_reader:
             print(row)
+            print()
 
-# defining to check inventory
-    def check_inventory(self, manufacturer, item_type, price, item_id):
-        self.manufacter = manufacturer
-        self.item_type = item_type
-        self.price = price
-        self.item_id = item_id
-        # prompting the user to enter a manufacturer and an item type
-        input_one = input('Please enter a manufacturer:')
-        input_two = input('Please enter an item type:')
-        if manufacturer and item_type not in CSVReader:
-            # this will print out whether or not the specified items are found
-            return 'No such item in inventory!'
-        else:
-            # this is the format to how it should be printing out if it is found within the csv file
-            print('Your item is: {}, {}, {}, {}').format(self.item_id, self.manufacterer, self.item_type, self.price)
-            print('You may, also, consider: {} {}, {}, {}').format(self.item_id, self.manufacter, self.item_type, self.price)
+    with open(file_two, 'r') as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=',')
+        for row in csv_reader:
+            print(row)
+            print()
 
-    # trying to loop back as a simple menu
-    def print_question(self):
-        print('\nWould you like to search for something else?\ny - Yes\nn - no (quit)')
-        option = input('\nPlease select an option:\n')
-        if option == 'y':
-            print(check_inventory)
-        if option == 'n':
+    with open(file_three, 'r') as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=',')
+        for row in csv_reader:
+            print(row)
+            print()
