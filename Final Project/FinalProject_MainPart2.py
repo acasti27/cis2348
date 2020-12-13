a79fd89b612b43ada06e0561bb7e59141fd7a581
acasti27 <70541336+acasti27@users.noreply.github.com>
2020-12-12 21:27:26 -0600

    Final Project part 2 - Went back to using lists and appending everything to that and then decided to switch everything to a while loop and fixed spacing

--- Final Project/FinalProject_MainPart2.py
+++ Final Project/FinalProject_MainPart2.py
@@ -1,22 +1,25 @@
 # Alejandra Castillo 1440370
 # This is the second part of my final project
-
 # this will read all the csv files
 import csv
 # this is for the group of files to append to
-# this dictionary has each list inside of it
-csv_files = {'manufacturer': [], 'prices': [], 'service_dates': []}
+csv_files = []
+manufacturer = []
+prices = []
+service_dates = []
 name_list = []
-user_input = []
+item_type = []
+usr_input = []
 item_id = []
 items = False
 manu = False
+isDamaged = False
+loop = True
 types = []
 brand = []
 
+while loop:
 
-# this class will be for reading the files
-class CSVReader:
     # prompting the user to input a file name in an order
     print('When entering a group of files to work with, please enter in the following order:\n '
           'Manufacturer, Price List, and Service Date List using the following format:\n'
@@ -29,66 +32,90 @@ class CSVReader:
     with open(input_file, 'r') as csvfile:
         csv_reader = csv.reader(csvfile, delimiter=',')
         for row in csv_reader:
-            csv_files['manufacturer'].append(row)
+            manufacturer.append(row)
+            csv_files.append(row)
 
     with open(file_two, 'r') as csvfile:
         csv_reader = csv.reader(csvfile, delimiter=',')
         for row in csv_reader:
-            csv_files['prices'].append(row)
+            prices.append(row)
+            csv_files.append(row)
 
     with open(file_three, 'r') as csvfile:
         csv_reader = csv.reader(csvfile, delimiter=',')
         for row in csv_reader:
-            csv_files['service_dates'].append(row)
+            service_dates.append(row)
+            csv_files.append(row)
 
 
-# this prints out the dictionary
-print(csv_files)
+        # printing out each appended list to make sure it's all printing
+        print(manufacturer)
+        print(prices)
+        print(service_dates)
+        # this prints out the dictionary
+        print(csv_files)
 
-# prompting the user to enter a manufacturer name and item type
-user_input = input('Please enter manufacturer and item type:\n')
-# this will split that input into to separate variables
-user_input = user_input.split()
-print(user_input)
-# taking those two and searching for them within the manufacturer list in the dictionary
-for i in range(len(user_input)):
-    if 'manufacturer'[i][1] in user_input:
-        manu = True
-    if 'manufacturer'[i][2] in user_input:
-        items = True
-# this is to check whether or not the item is damaged, I opted to display a message in place in case it is
-if manu is True and items is True:
-    for i in range(len(user_input)):
-        if 'manufacturer'[i][1] in user_input and 'manufacturer'[i][2] in user_input:
-            print('manufacturer'[i])
-            if 'manufacturer'[i][3] == 'damaged':
-                isDamaged = True
-                print("This item is damaged. \n")
-                break
+        # prompting the user to enter a manufacturer name and item type
+        user_input = input('Please enter manufacturer and item type:\n')
+        # this will split that input into to separate variables
+        user_input = user_input.split()
+        print(user_input)
+        # taking those two and searching for them within the manufacturer list in the dictionary
+        for i in range(len(user_input)):
+            if manufacturer[i][1] in user_input:
+                manu = True
+            if manufacturer[i][2] in user_input:
+                items = True
+         # this is to check whether or not the item is damaged, I opted to display a message in place in case it is
+        if manu == True and items == True:
+            for i in range(len(manufacturer)):
+                if manufacturer[i][1] in user_input and manufacturer[i][2] in user_input:
+                    if manufacturer[i][3] == 'damaged':
+                        isDamaged = True
+                        print("This item is damaged. \n")
+                        break
             # if the item is not damaged, then the idea is to store the information into the list
             else:
-                item_id.append('manufacturer'[i][0])
-                brand.append('manufacturer'[i][1])
-                types.append('manufacturer'[i][2])
+                item_id.append(manufacturer[i][0])
+                brand.append(manufacturer[i][1])
+                types.append(manufacturer[i][2])
                 print('items have been copied\n')
-else:
-    print('This item is not in our inventory.\n')
+        else:
+            print('This item is not in our inventory.\n')
 
-if isDamaged == False:
-    for i in range(len('manufacturer')):
-        # finds info of item
-        if 'manufacturer'[i][0] in item_id:
-            print('manufacturer'[i])
-        # finds price of item
-        if 'prices'[i][0] in item_id:
-            print('prices'[i])
-while True:
+        if isDamaged == False:
+            for i in range(len(manufacturer)):
+                # finds info of item
+                    if manufacturer[i][0] in item_id:
+                         print(manufacturer[i])
+    # finds price of item
+                    if prices[i][0] in item_id:
+                        print('prices'[i])
+
+
+
     # this will ask the user to search again or quit
     print('\nWould you like to search for something else?\ny - yes\nq - quit')
     option = input('Please select an option:\n')
     # this will prompt the user to search for something else again
     if option == 'y':
-        print()
-    # this will quit the program
-    if option == 'q':
+        csv_files = []
+        manufacturer = []
+        prices = []
+        service_dates = []
+        name_list = []
+        item_type = []
+        usr_input = []
+        item_id = []
+        items = False
+        manu = False
+        isDamaged = False
+        loop = True
+        types = []
+        brand = []
+    elif option == 'q':
+        loop is False
+        break
+    else:
+        print('That is not an option. Goodbye.')
         break
