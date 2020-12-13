71c3fd7923bcb16fad340af8a50406dce78f4efe
acasti27 <70541336+acasti27@users.noreply.github.com>
2020-12-12 23:11:17 -0600

    Final Project part 2 - finished comments,fixed formatting,made sure that it could suggest an alternative

--- Final Project/FinalProject_MainPart2.py
+++ Final Project/FinalProject_MainPart2.py
@@ -7,6 +7,7 @@ csv_files = []
 manufacturer = []
 prices = []
 service_dates = []
+# these are all lists for the various variables and statements for later
 name_list = []
 item_type = []
 usr_input = []
@@ -17,6 +18,7 @@ isDamaged = False
 loop = True
 types = []
 brand = []
+alternative_items = []
 
 while loop:
 
@@ -33,19 +35,16 @@ while loop:
         csv_reader = csv.reader(csvfile, delimiter=',')
         for row in csv_reader:
             manufacturer.append(row)
-            csv_files.append(row)
 
     with open(file_two, 'r') as csvfile:
         csv_reader = csv.reader(csvfile, delimiter=',')
         for row in csv_reader:
             prices.append(row)
-            csv_files.append(row)
 
     with open(file_three, 'r') as csvfile:
         csv_reader = csv.reader(csvfile, delimiter=',')
         for row in csv_reader:
             service_dates.append(row)
-            csv_files.append(row)
 
 
         # printing out each appended list to make sure it's all printing
@@ -53,15 +52,13 @@ while loop:
         print(prices)
         print(service_dates)
         # this prints out the dictionary
-        print(csv_files)
-
         # prompting the user to enter a manufacturer name and item type
         user_input = input('Please enter manufacturer and item type:\n')
         # this will split that input into to separate variables
         user_input = user_input.split()
         print(user_input)
         # taking those two and searching for them within the manufacturer list in the dictionary
-        for i in range(len(user_input)):
+        for i in range(len(manufacturer)):
             if manufacturer[i][1] in user_input:
                 manu = True
             if manufacturer[i][2] in user_input:
@@ -69,30 +66,46 @@ while loop:
          # this is to check whether or not the item is damaged, I opted to display a message in place in case it is
         if manu == True and items == True:
             for i in range(len(manufacturer)):
+                # this is to look for both inputs
                 if manufacturer[i][1] in user_input and manufacturer[i][2] in user_input:
                     if manufacturer[i][3] == 'damaged':
                         isDamaged = True
                         print("This item is damaged. \n")
                         break
             # if the item is not damaged, then the idea is to store the information into the list
-            else:
-                item_id.append(manufacturer[i][0])
-                brand.append(manufacturer[i][1])
-                types.append(manufacturer[i][2])
-                print('items have been copied\n')
+                    else:
+                        item_id.append(manufacturer[i][0])
+                        brand.append(manufacturer[i][1])
+                        types.append(manufacturer[i][2])
+                        print('Items have been copied.\n')
         else:
-            print('This item is not in our inventory.\n')
-
+            print('This item is not in our inventory.')
+        # if the item is not damaged the it'll find the information for it
         if isDamaged == False:
+            print('Your item is:\n')
             for i in range(len(manufacturer)):
                 # finds info of item
-                    if manufacturer[i][0] in item_id:
-                         print(manufacturer[i])
-                    # finds price of item
-                    if prices[i][0] in item_id:
-                        print('prices'[i])
-
-
+                if manufacturer[i][0] in item_id:
+                    print(manufacturer[i])
+                # finds price of item
+                if prices[i][0] in item_id:
+                    print(prices[i])
+        # this is for when the alternative is damaged
+        for i in range(len(manufacturer)):
+            if manufacturer[i][2] in types and manufacturer[i][1] not in brand:
+                if manufacturer[i][3] == 'damaged':
+                    continue
+                else:
+                    alternative_items.append(manufacturer[i][0])
+        # for the alternative item
+        if len(alternative_items) > 0:
+            print('You may also consider:\n')
+            for i in range(len(manufacturer)):
+                if manufacturer[i][0] in alternative_items:
+                    print(manufacturer[i])
+                    # this will find the price of the item
+                if prices[i][0] in alternative_items:
+                    print(prices[i])
 
     # this will ask the user to search again or quit
     print('\nWould you like to search for something else?\ny - yes\nq - quit')
